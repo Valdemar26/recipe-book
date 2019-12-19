ed49b7c1ba6ff1f74a5020250dfed20e90f132ed
Volodymyr Zaplatynskii <v.zaplatunskuj@crosssoft.de>
2019-12-19 16:45:07 +0200

    3: create recipe-item

--- src/app/service/recipe-book.service.ts
+++ src/app/service/recipe-book.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-
-import { delay } from 'rxjs/operators';
+import { delay, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -80,14 +78,118 @@ export class RecipeBookService {
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam consectetur' +
       ' consequuntur delectus, eligendi error expedita illum iste iusto maxime necessitatibus, officiis praesentium' +
       ' similique, ullam vero. Illo.'
+    },
+    {
+      title: 'Recipe 10',
+      image: 'http://lorempixel.com/output/food-q-c-640-480-8.jpg',
+      id: 10,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam consectetur' +
+        ' consequuntur delectus, eligendi error expedita illum iste iusto maxime necessitatibus, officiis praesentium' +
+        ' similique, ullam vero. Illo.'
+    },
+    {
+      title: 'Recipe 11',
+      image: 'http://lorempixel.com/output/food-q-c-640-480-8.jpg',
+      id: 11,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam consectetur consequuntur ' +
+        'delectus, eligendi error expedita illum iste iusto maxime necessitatibus, officiis praesentium similique, ullam vero.' +
+        ' Illo.'
+    },
+    {
+      title: 'Recipe 12',
+      image: 'http://lorempixel.com/output/food-q-c-640-480-6.jpg',
+      id: 12,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam consectetur consequuntur' +
+        ' delectus, eligendi error expedita illum iste iusto maxime necessitatibus, officiis praesentium similique,' +
+        ' ullam vero. Illo.'
+    },
+    {
+      title: 'Recipe 13',
+      image: 'http://lorempixel.com/output/food-q-c-640-480-9.jpg',
+      id: 13,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam consectetur' +
+        ' consequuntur delectus, eligendi error expedita illum iste iusto maxime necessitatibus, officiis praesentium' +
+        ' similique, ullam vero. Illo.'
+    },
+    {
+      title: 'Recipe 14',
+      image: 'http://lorempixel.com/output/food-q-c-640-480-4.jpg',
+      id: 14,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam consectetur' +
+        ' consequuntur delectus, eligendi error expedita illum iste iusto maxime necessitatibus, officiis praesentium' +
+        ' similique, ullam vero. Illo.'
+    },
+    {
+      title: 'Recipe 15',
+      image: 'http://lorempixel.com/output/food-q-c-640-480-10.jpg',
+      id: 15,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam consectetur' +
+        ' consequuntur delectus, eligendi error expedita illum iste iusto maxime necessitatibus, officiis praesentium' +
+        ' similique, ullam vero. Illo.'
+    },
+    {
+      title: 'Recipe 16',
+      image: 'http://lorempixel.com/output/food-q-c-640-480-7.jpg',
+      id: 16,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam consectetur' +
+        ' consequuntur delectus, eligendi error expedita illum iste iusto maxime necessitatibus, officiis praesentium' +
+        ' similique, ullam vero. Illo.'
+    },
+    {
+      title: 'Recipe 17',
+      image: 'http://lorempixel.com/output/food-q-c-640-480-2.jpg',
+      id: 17,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam consectetur' +
+        ' consequuntur delectus, eligendi error expedita illum iste iusto maxime necessitatibus, officiis praesentium' +
+        ' similique, ullam vero. Illo.'
+    },
+    {
+      title: 'Recipe 18',
+      image: 'http://lorempixel.com/output/abstract-q-c-640-480-10.jpg',
+      id: 18,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam consectetur' +
+        ' consequuntur delectus, eligendi error expedita illum iste iusto maxime necessitatibus, officiis praesentium' +
+        ' similique, ullam vero. Illo.'
+    },
+    {
+      title: 'Recipe 19',
+      image: 'http://lorempixel.com/output/abstract-q-c-640-480-1.jpg',
+      id: 19,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam consectetur' +
+        ' consequuntur delectus, eligendi error expedita illum iste iusto maxime necessitatibus, officiis praesentium' +
+        ' similique, ullam vero. Illo.'
+    },
+    {
+      title: 'Recipe 20',
+      image: 'http://lorempixel.com/output/food-q-c-640-480-8.jpg',
+      id: 20,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam consectetur' +
+        ' consequuntur delectus, eligendi error expedita illum iste iusto maxime necessitatibus, officiis praesentium' +
+        ' similique, ullam vero. Illo.'
+    },
+    {
+      title: 'Recipe 21',
+      image: 'http://lorempixel.com/output/food-q-c-640-480-8.jpg',
+      id: 21,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam consectetur' +
+        ' consequuntur delectus, eligendi error expedita illum iste iusto maxime necessitatibus, officiis praesentium' +
+        ' similique, ullam vero. Illo.'
     }
   ];
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   public getRecipeList(): Observable<any> {
     return of(this.recipes).pipe(
       delay(700)
+    );
+  }
+
+  getRecipeById(id: number) {
+    return of(this.recipes).pipe(
+      map((res: any) => {
+        return res.filter(item => item.id === id);
+      })
     );
   }
 }
